140b05e84875ae762458f4e76f0be321595ff1cf
Menachem Aber <abermendel@gmail.com>
2018-12-11 21:55:31 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(the_name)
+    @name = the_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_type)
+    @job = job_type
+  end
+  
+  def job
+    @job
+  end
+  
+end
