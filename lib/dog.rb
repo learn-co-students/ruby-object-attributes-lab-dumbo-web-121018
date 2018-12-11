e69e53d54c2865860da28a5a8bcce8e80a5595b2
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-12-11 22:03:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name
+    @name 
+  end 
+  
+  def name= (given_name)
+    @name = given_name
+  end
+  
+  def breed 
+    @breed
+  end 
+  
+  def breed= (born_breed)
+    @breed = born_breed
+  end
+end 
+
