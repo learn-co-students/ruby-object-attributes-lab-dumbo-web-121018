9825be1e8080e2aeb18c6513eb9d7d7be5194007
rhuwellsamano <rhuwell.samano@gmail.com>
2018-12-12 13:44:43 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,39 @@
+class Dog
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
+# fido = Dog.new
+# fido.name = "Fido"
+
+#   class Person
+#
+#   def initialize(first_name, last_name)
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+#
+#   def name=(full_name)
+#     first_name, last_name = full_name.split
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+#
+#   def name
+#     "#{@first_name} #{@last_name}".strip
+#   end
+#
+# end
