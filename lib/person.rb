c135377488d7c0a98d2f32a0fc73ad32bbf8c047
Joshua Alphonse <joshalphonse@gmail.com>
2018-12-12 18:51:19 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,24 @@
+require'pry'
+class Person
+
+  def name
+    @name
+  end
+
+  def name=(new_person)
+    @name = new_person
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job=new_job
+  end
+
+
+end
+
+fido = Person.new
+fido.name
